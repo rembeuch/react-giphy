cc22356c5346ac3c6cc52f3a22728b7f2a617f38
beucherie remi <beucherie.r@gmail.com>
2020-12-25 20:45:13 +0100

    h1 clicable

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+
+class Hello extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      clicked: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  };
+
+  render(){
+    return(
+        <div className={this.state.clicked ? 'clicked' : null }
+        onClick={this.handleClick}>
+          Hello {this.props.name}!
+        </div>
+      )
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
